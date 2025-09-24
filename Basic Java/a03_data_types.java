5425133c069b86e41d8d70978ef10541e436d5c7
manishkumar0089 <143782757+manishkumar0089@users.noreply.github.com>
2025-09-24 14:29:06 +0530

    Add files via upload

---
+++ Basic Java/a03_data_types.java
@@ -0,0 +1,11 @@
+public class a03_data_types {
+    public static void main(String[] args){
+        System.out.print("sum of three number is : ");
+        int num1 = 10;
+        int num2 = 98;
+        int num3 = 98;
+        int sum = num1 + num2 +num3;
+        System.out.print(sum);
+    }
+    
+}
