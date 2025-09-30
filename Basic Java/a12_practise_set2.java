33b82b32cec2c2575df8cb70988d28b74bcea399
manishkumar0089 <143782757+manishkumar0089@users.noreply.github.com>
2025-09-30 12:42:43 +0530

    Add files via upload

---
+++ Basic Java/a12_practise_set2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class a12_practise_set2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int b = input.nextInt();
+        System.out.println(b>8);
+        float v =8, u = 1, a = 2, s = 3;
+        float exp = (v*v -u*u)/(2*a*s);
+        System.out.println(exp);
+        
+    }
+}
---
+++ Basic Java/a13_intro_2_strings.java
@@ -0,0 +1,7 @@
+public class a13_intro_2_strings {
+    public static void main(String[] args) {
+        String new1 = "Manish is my name";
+        System.out.println(new1);
+
+    }
+}
